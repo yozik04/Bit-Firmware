ffca205a405fe474658fcac654cb19e0e832ca65
Emil Gajšak <emil.gajsak@gmail.com>
2023-08-08 17:40:47 +0200

    Fixed TextRC pushing, split setters for text and style

--- main/src/GameEngine/Rendering/TextRC.cpp
+++ main/src/GameEngine/Rendering/TextRC.cpp
@@ -4,16 +4,18 @@ TextRC::TextRC(std::string text, TextStyle style) : text(text), style(style){
 }
 
 void TextRC::push(Sprite& parent, PixelDim pos, float rot) const{
-	parent.setCursor(pos.x, pos.y);
 	parent.setFont(style.font);
 	parent.setTextDatum(style.datum);
 	parent.setTextColor(style.color);
 	parent.setTextSize(style.scale);
 
-	parent.print(text.c_str());
+	parent.drawString(text.c_str(), pos.x, pos.y);
 }
 
-void TextRC::setText(std::string text, TextStyle style){
+void TextRC::setText(std::string text){
 	this->text = text;
+}
+
+void TextRC::setStyle(TextStyle style){
 	this->style = style;
 }
