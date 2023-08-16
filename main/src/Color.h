9842db6a786b3a7c4defea1a3215ce896c50f3d5
Emil Gajšak <emil.gajsak@gmail.com>
2023-08-16 13:25:40 +0200

    Edited Color.h functions to be constexpr

--- main/src/Color.h
+++ main/src/Color.h
@@ -13,7 +13,16 @@ struct Pixel {
 	}
 };
 
-Color C_RGB(uint8_t r, uint8_t g, uint8_t b);
+constexpr Color C_RGB(uint8_t r, uint8_t g, uint8_t b){
+	return (((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3));
+}
 
-Color C_HEX(uint32_t hex);
+constexpr Color C_HEX(uint32_t hex){
+	return C_RGB(
+			(hex >> 16) & 0xFF,
+			(hex >> 8) & 0xFF,
+			hex & 0xFF
+	);
+}
+
 #endif //BIT_FIRMWARE_COLOR_H
