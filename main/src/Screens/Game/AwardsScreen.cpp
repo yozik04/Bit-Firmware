92fcfaed61ebf0df3d9dcd1faf201b5a46e8b444
Fr0zen3k <jan.kresic@circuitmess.com>
2024-07-11 15:53:23 +0200

    Award screen xp bar alignment fixed BIT-132

--- main/src/Screens/Game/AwardsScreen.cpp
+++ main/src/Screens/Game/AwardsScreen.cpp
@@ -190,7 +190,7 @@ void AwardsScreen::setAwardMode(Award award){
 		lv_obj_set_style_bg_img_src(bar, Filepath::Award::XpBackground, 0);
 		lv_obj_set_style_bg_opa(bar, 100, 0);
 
-		xpBar = new XPBar(XPBarLength::Short, bar, xpSystem->MapXPToLevel(xpSystem->getXP()).progress);
+		xpBar = new XPBar(XPBarLength::Medium, bar, xpSystem->MapXPToLevel(xpSystem->getXP()).progress);
 
 		const LevelProgress progress = xpSystem->MapXPToLevel(xpSystem->getXP() + xp);
 		xpBar->setFill(progress.nextLvl > levelSet + 1 ? 1.0f : progress.progress, true);
@@ -233,7 +233,7 @@ void AwardsScreen::setAwardMode(Award award){
 		lv_obj_set_style_bg_img_src(bar, Filepath::Award::XpBackground, 0);
 		lv_obj_set_style_bg_opa(bar, 100, 0);
 
-		xpBar = new XPBar(XPBarLength::Short, bar, 0.0f);
+		xpBar = new XPBar(XPBarLength::Medium, bar, 0.0f);
 		lv_obj_set_align(*xpBar, LV_ALIGN_CENTER);
 	}else if(award == Award::Achievement){
 		// TODO: init achievement unlocked up UI
