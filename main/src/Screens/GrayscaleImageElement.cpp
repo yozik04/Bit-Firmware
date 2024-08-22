175a774b8f0f864f393c2ef97a6a069d0fdaee60
Filip Budiša <filip@circuitmess.com>
2024-08-22 17:23:29 +0200

    Remove caching from GrayscaleImageElement

--- main/src/Screens/GrayscaleImageElement.cpp
+++ main/src/Screens/GrayscaleImageElement.cpp
@@ -16,12 +16,8 @@ void GrayscaleImageElement::setGrayscale(bool value){
 	grayscale = value;
 
 	if(grayscale){
-		FSLVGL::removeFromCache(path.c_str());
-		FSLVGL::addToCache(pathGrayscale.c_str());
 		lv_img_set_src(img, pathGrayscale.c_str());
 	}else{
-		FSLVGL::removeFromCache(pathGrayscale.c_str());
-		FSLVGL::addToCache(path.c_str());
 		lv_img_set_src(img, path.c_str());
 	}
 }
