42b1da33ea7045143d24b4dab96ac0c1c670d90d
Filip Budiša <filip@circuitmess.com>
2024-08-25 15:39:46 +0200

    Add currLevel to LevelProgress, add it to XPSystem::MapXPToLevel

--- main/src/Services/XPSystem.h
+++ main/src/Services/XPSystem.h
@@ -6,6 +6,7 @@
 struct LevelProgress {
 	uint8_t nextLvl; //from 2 to MaxLevel
 	float progress; //0.0 - 1.0
+	uint8_t currLvl;
 };
 
 class XPSystem {
@@ -17,16 +18,20 @@ public:
 
 	inline static constexpr LevelProgress MapXPToLevel(uint32_t xp) {
 		const uint8_t levelupsNum = sizeof(LevelupThresholds) / sizeof(uint32_t);
+		uint8_t curLvl = 1;
 
 		for(uint8_t i = 0; i < levelupsNum; i++){
 			if(xp < LevelupThresholds[i]){
-				return { (uint8_t) (i + 2), (float) xp / LevelupThresholds[i] };
+				return { (uint8_t) (i + 2), (float) xp / LevelupThresholds[i], curLvl };
 			}
 
 			xp -= LevelupThresholds[i];
+			curLvl++;
 		}
-		return { MaxLevel, 1.0f };
+		return { MaxLevel, 1.0f, 5 };
 	}
+
+	static constexpr uint8_t MaxLevel = 5;
 
 private:
 	NVSFlash& nvs;
@@ -37,7 +42,6 @@ private:
 
 	static constexpr const char* BlobName = "XP";
 
-	static constexpr uint8_t MaxLevel = 5;
 	static constexpr uint32_t LevelupThresholds[MaxLevel - 1] = { 200, 400, 1000, 2000 };
 };
 
